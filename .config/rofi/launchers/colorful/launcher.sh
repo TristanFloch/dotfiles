079d5cc01b1b8505f62889ece6dcd7d7c21c0da9
Tristan FLOCH <tristan.floch@epita.fr>
2021-08-28 12:59:26 +0200

    updated rofi config to dracula theme

--- .config/rofi/launchers/colorful/launcher.sh
+++ .config/rofi/launchers/colorful/launcher.sh
@@ -11,14 +11,14 @@
 # style_1     style_2     style_3     style_4     style_5     style_6
 # style_7     style_8     style_9     style_10    style_11    style_12
 
-theme="style_2"
+theme="style_1"
 dir="$HOME/.config/rofi/launchers/colorful"
 
 # dark
 ALPHA="#00000000"
-BG="#000000ff"
+BG="#1e2029ff"
 FG="#FFFFFFff"
-SELECT="#101010ff"
+SELECT="#f8f8f2ff"
 
 # light
 #ALPHA="#00000000"
@@ -27,9 +27,9 @@ SELECT="#101010ff"
 #SELECT="#f3f3f3ff"
 
 # accent colors
-COLORS=('#EC7875' '#61C766' '#FDD835' '#42A5F5' '#BA68C8' '#4DD0E1' '#00B19F' \
-		'#FBC02D' '#E57C46' '#AC8476' '#6D8895' '#EC407A' '#B9C244' '#6C77BB')
-ACCENT="${COLORS[$(( $RANDOM % 14 ))]}ff"
+# COLORS=('#bd93f9' '#8be9fd' '#ff79c6' '#ff5555' '#f1fa8c' '#ffb86c' '#50fa7b')
+# ACCENT="${COLORS[$(( $RANDOM % 7 ))]}ff"
+ACCENT=#bd93f9
 
 # overwrite colors file
 cat > $dir/colors.rasi <<- EOF
@@ -45,7 +45,7 @@ cat > $dir/colors.rasi <<- EOF
 EOF
 
 # comment these lines to disable random style
-themes=($(ls -p --hide="launcher.sh" --hide="colors.rasi" $dir))
-theme="${themes[$(( $RANDOM % 12 ))]}"
+# themes=($(ls -p --hide="launcher.sh" --hide="colors.rasi" $dir))
+# theme="${themes[$(( $RANDOM % 12 ))]}"
 
 rofi -no-lazy-grab -show drun -modi drun -theme $dir/"$theme"
--- .config/rofi/launchers/misc/launcher.sh
+++ .config/rofi/launchers/misc/launcher.sh
@@ -9,10 +9,10 @@
 # >> Created and tested on : rofi 1.6.0-1
 #
 # blurry	blurry_full		kde_simplemenu		kde_krunner		launchpad
-# gnome_do	slingshot		appdrawer			appdrawer_alt	appfolder
-# column	row				row_center			screen			row_dock		row_dropdown
+# gnome_do	slingshot		appdrawer		appdrawer_alt	        appfolder
+# column	row			row_center		screen			row_dock		row_dropdown
 
-theme="blurry"
+theme="launchpad"
 dir="$HOME/.config/rofi/launchers/misc"
 
 # comment these lines to disable random style
